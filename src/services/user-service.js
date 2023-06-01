1a057eb21d69d975d4d90aa5e6f962de24e506d4
Sanket Singh <singhsanket143@gmail.com>
2023-06-01 22:48:20 +0530

    Added jwt expiry error handling

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -57,6 +57,9 @@ async function isAuthenticated(token) {
         if(error.name == 'JsonWebTokenError') {
             throw new AppError('Invalid JWT token', StatusCodes.BAD_REQUEST);
         }
+        if(error.name == 'TokenExpiredError') {
+            throw new AppError('JWT token expired', StatusCodes.BAD_REQUEST);
+        }
         console.log(error);
         throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
     }
